fd9f1ad40442cce584ada5b613294d9bc05c46ba
Zoe  Markovits <zoemarkovits@Zoes-MacBook-Air.local>
2019-03-27 13:56:22 -0400

    Assignment Two

--- Assignment Two/NB-extended.py
+++ Assignment Two/NB-extended.py
@@ -1,17 +1,53 @@
+
+"""Training additional model:"""
+
 import numpy as np
 import pickle
 import os
 import glob
 import re
+import nltk
+from nltk.corpus import stopwords
 
-def get_vocab_size(vocab_file = 'aclImdb/imdb.vocab'):
+# Write a function that creates a new vocabulary that drops stopwords
+def new_vocab(vocab_file = 'aclImdb/imdb.vocab'):
+    characters = ["!","?","(",")",":",";"]
+    stoplist = stopwords.words('english') + characters
+    new_vocab_list = []
+    with open(vocab_file, 'rb') as file:
+        for line in file:
+            line = str(line)
+            line = line[2:-3]
+            line = ' '.join([word for word in line.split() if word not in stoplist])
+            new_vocab_list.append(line)
+    n=''
+    for i in new_vocab_list:
+        if(i==n):
+            new_vocab_list.remove(i)
+
+    with open('new_imdb.vocab', 'wb') as file:
+        pickle.dump(new_vocab_list, file)
+
+    return new_vocab_list
+
+new_vocab()
+
+with open('new_imdb.vocab', 'rb') as file:
+    new_vocabulary = pickle.load(file)
+
+
+# Running our get vocab size function from NB.py except with new vocab file
+def get_vocab_size(vocab_file = 'new_imdb.vocab'):
     sum = 0
     with open(vocab_file, 'rb') as file:
         for line in file:
             sum += 1
     return sum
 
-def fit(pos_pickle='train_pos.pickle', neg_pickle='train_neg.pickle', vocab_file = 'aclImdb/imdb.vocab'):
+get_vocab_size()
+
+# Running our fit function from NB.py except with new vocab file and pickling movie-review-extended.NB
+def fit(pos_pickle='train_pos.pickle', neg_pickle='train_neg.pickle', vocab_file = 'new_imdb.vocab'):
     with open(pos_pickle, 'rb') as file:
         pos_dict = pickle.load(file)
 
@@ -40,9 +76,12 @@ def fit(pos_pickle='train_pos.pickle', neg_pickle='train_neg.pickle', vocab_file
     prob_dict['neg']['smooth_value'] = np.log2(1/neg_sum)
     prob_dict['pos']['smooth_value'] = np.log2(1/pos_sum)
 
+    with open('movie-review-extended.NB', 'wb') as file:
+        pickle.dump(prob_dict, file)
+
     return prob_dict
 
-
+# Running our predict function from NB.py
 def predict(filename):
     word_count_dict = {}
 
@@ -80,7 +119,7 @@ def predict(filename):
     else:
         return 'pos'
 
-
+# Running our score function from NB.py
 def score(folder = 'aclImdb/test'):
     pos_folder = folder + '/pos'
     neg_folder = folder + '/neg'
@@ -107,60 +146,5 @@ def score(folder = 'aclImdb/test'):
 
 # Results
 prob_dict = fit()
-small_prob_dict = fit(pos_pickle='small_pos.pickle', neg_pickle='small_neg.pickle')
-
-with open('movie-review-BOW.NB', 'wb') as file:
-    pickle.dump(prob_dict, file)
-
-with open('movie-review-small.NB', 'wb') as file:
-    pickle.dump(small_prob_dict, file)
-
 prob_dict['pos']['class_prior']
 wrong_files = score()
-
-# Investigate results and see if we find trends
-
-first_ten = wrong_files['false pos'][:10]
-#first_ten = wrong_files['false neg'][:10]
-
-for line in first_ten:
-    with open(line) as file:
-        for l in file:
-            print()
-            print(l)
-            print(bayes_probs(line))
-
-def bayes_probs(filename):
-
-    word_count_dict = {}
-
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            line = str(line)
-            line = line.lower()
-            line = re.sub('<[^<]+?>', '', line)
-            new_line = ''
-            for i in line:
-                if i not in '!"#$%&()*+,./:;<=>?@[]^_`{|}~':
-                    new_line += i
-            line = new_line
-            # append each train file
-            for word in line.split():
-                try:
-                    word_count_dict[word] += 1
-                except KeyError:
-                    word_count_dict[word] = 1
-
-    conditional_positive = prob_dict['pos']['class_prior']
-    conditional_negative = prob_dict['neg']['class_prior']
-    print(word_count_dict)
-    for key in word_count_dict.keys():
-        try:
-            conditional_positive += prob_dict['pos'][key] * word_count_dict[key]
-        except KeyError:
-            conditional_positive += prob_dict['pos']['smooth_value']
-        try:
-            conditional_negative += prob_dict['neg'][key] * word_count_dict[key]
-        except KeyError:
-            conditional_negative += prob_dict['neg']['smooth_value']
-    print(conditional_negative, conditional_positive)
